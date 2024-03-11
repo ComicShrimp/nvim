fe358e343f711c17e9d0e42914f28b4edba45a1e
Mário Victor Ribeiro Silva <mariovictorrs@gmail.com>
2024-03-11 16:57:08 -0300

    chore: lsp_zero config and telescope

--- lua/mario/plugins/lsp_zero.lua
+++ lua/mario/plugins/lsp_zero.lua
@@ -7,8 +7,13 @@ return {
 	{"neovim/nvim-lspconfig"},
 	{"hrsh7th/cmp-nvim-lsp"},
 	{"hrsh7th/nvim-cmp"},
-	{"L3MON4D3/LuaSnip"},
-	--- Uncomment the two plugins below if you want to manage the language servers from neovim
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
+    },
 	{"williamboman/mason.nvim"},
 	{"williamboman/mason-lspconfig.nvim"},
 	{
@@ -45,7 +50,7 @@ return {
 					"lua_ls",
                     -- "luacheck", disabled because it dont have an entry in mason-lspconfig.nvim. Must be installed manually
                     -- Python
-                    -- "ruff", disabled because it dont have an entry in mason-lspconfig.nvim. Must be installed manually
+                    -- "pyright", disabled because it dont have an entry in mason-lspconfig.nvim. Must be installed manually
 					"ruff_lsp",
                     -- Docker
 					"dockerls",
@@ -92,15 +97,15 @@ return {
                 local opts = {buffer = bufnr, remap = false}
 
                 vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts, "Goto definition")
-                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-                vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
-                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+                vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts, "Show hover")
+                vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts, "Search workspace symbols")
+                vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts, "Open diagnostics")
+                vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts, "Next diagnostic")
+                vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts, "Previous diagnostic")
+                vim.keymap.set("n", "<leader>ca", function() vim.lsp.buf.code_action() end, opts, "Code action")
+                vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts, "Find references")
+                vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts, "Rename")
+                vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts, "Signature help")
             end)
 
             lsp_zero.setup()
